3dc1148c143a41e4387bffedb8b0ff6b34bce5fe
Alibek1207 <alibek20071277@gmail.com>
2024-07-04 21:31:51 +0500

    menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 import logo from "../../assets/Logo/logo.png"
+import menu from "../../assets/nav/menuIcon.png"
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -15,11 +16,7 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
+          src={menu}
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
         />
